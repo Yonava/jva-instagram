75b030bdc86dd35320d688f9f137fa3060d0805b
yonava <yonavossandreae@gmail.com>
2026-04-25 13:12:59 -0700

    inquiry added

--- src/detail/SculptureOverlay.vue
+++ src/detail/SculptureOverlay.vue
@@ -5,22 +5,31 @@ import type { Sculpture } from '../useSculptureData'
 import { viewTransitionName } from '../utils'
 import SculptureSwiper from './SculptureSwiper.vue'
 
-defineProps<{
+const props = defineProps<{
   sculpture: Sculpture
 }>()
 
 const emits = defineEmits<{
   (e: 'back-out'): void
 }>()
+
+const inquirySubjectLine = props.sculpture.title ? encodeURIComponent(`Inquire About ${props.sculpture.title}`) : undefined;
+const inquiryEmail = 'office@julianvossandreae.com';
+const inquiryUrl = `mailto:${inquiryEmail}${inquirySubjectLine ? `?subject=${inquirySubjectLine}` : ''}`
 </script>
 
 <template>
   <div class="absolute w-full h-full flex flex-col items-center">
     <div class="h-full w-full" :style="{ viewTransitionName: viewTransitionName(sculpture) }">
-      <div class="absolute top-0 p-6 z-10">
-        <button @click="emits('back-out')">
-          <ArrowBack :size="48" class="text-white"></ArrowBack>
-        </button>
+      <div class="absolute top-0 p-6 z-10 w-full">
+        <div class="flex justify-between items-center">
+          <button @click="emits('back-out')">
+            <ArrowBack :size="48" class="text-white"></ArrowBack>
+          </button>
+          <a :href="inquiryUrl" class="bg-gray-800 text-xl text-gray-100 px-8 py-2 rounded-full">
+            Inquire
+          </a>
+        </div>
       </div>
       <SculptureSwiper @back-out="emits('back-out')" :sculpture="sculpture" />
     </div>
